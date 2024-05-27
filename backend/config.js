8734f6bfe3513947c8255f616bc5a28db078093f
rupashreesmagic <rupashreesmagic@gmail.com>
2024-05-27 10:13:04 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+export const PORT = 5555;
+
+//export const mongoDBURL = 'mongodb+srv://rupashreesmagic:RUPraj@231@rupashreedb.7w0ynmg.mongodb.net/books-collection?retryWrites=true&w=majority&appName=RupashreeDB';
+
+//mongodb+srv://rupashreesmagic:<password>@rupashreedb.7w0ynmg.mongodb.net/?retryWrites=true&w=majority&appName=RupashreeDB///
+
+//N9bBaaHuSUTUD0JU - Password
+
+export const mongoDBURL =  "mongodb+srv://rupashreesmagic:N9bBaaHuSUTUD0JU@bookstore.p4yagn1.mongodb.net/?retryWrites=true&w=majority&appName=bookstore";
